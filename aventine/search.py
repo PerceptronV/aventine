8b68e0ce6ff270d057330cb40e153ebb580f4d92
PerceptronV <neutrinovs@gmail.com>
2024-12-20 14:45:48 -0500

    Even better error handling

--- aventine/search.py
+++ aventine/search.py
@@ -7,27 +7,32 @@ bp = Blueprint('search', __name__, url_prefix='/search')
 
 @bp.route('/')
 def search():
-    query = request.args.get('query')
-    language = request.args.get('language').strip()
-    if request.args.get('texts') is None or request.args.get('texts') == 'ALL':
-        texts = None
-    else:
-        texts = {i.strip() for i in request.args.get('texts').split(',')}
-    results = int(n) if (n := request.args.get('results')) is not None else 50
+    try:
+        query = request.args.get('query')
+        language = request.args.get('language').strip()
+        if request.args.get('texts') is None or request.args.get('texts') == 'ALL':
+            texts = None
+        else:
+            texts = {i.strip() for i in request.args.get('texts').split(',')}
+        results = int(n) if (n := request.args.get('results')) is not None else 50
 
-    data = engine.search(query=query,
-                         language=language,
-                         texts=texts,
-                         results=results)
-    g.query = query
-    g.language = language
-    g.texts = ', '.join([id for id in texts]) if texts is not None else 'ALL'
-    g.titles = ', '.join([engine.id2title[id] for id in texts]) if texts is not None else 'ALL'
-    g.data = data
-    g.id2title = engine.id2title
-    g.to_percentage = lambda x : f"{x:.3%}"
+        data = engine.search(query=query,
+                            language=language,
+                            texts=texts,
+                            results=results)
+        g.query = query
+        g.language = language
+        g.texts = ', '.join([id for id in texts]) if texts is not None else 'ALL'
+        g.titles = ', '.join([engine.id2title[id] for id in texts]) if texts is not None else 'ALL'
+        g.data = data
+        g.id2title = engine.id2title
+        g.to_percentage = lambda x : f"{x:.3%}"
 
-    if data is None:
-        return render_template('error.html')
-    else:
-        return render_template('search.html')
+        if data is None:
+            return render_template('error.html')
+        else:
+            return render_template('search.html')
+
+    except:
+        return 'O TEMPORA! O MORES!\nSomething has gone terribly wrong.'
+    
